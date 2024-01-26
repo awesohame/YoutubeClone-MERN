400f66b60d1917c033747702c68985346d6ffcb4
Awesohame <82087247+awesohame@users.noreply.github.com>
2024-01-26 12:20:52 +0530

    intializing backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
+import Navbar from "./components/Navbar";
+import Video from "./pages/Video";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-
-
   return (
     <>
-
+      {/* <Navbar /> */}
+      <Video />
     </>
   )
 }
