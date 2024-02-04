b9c62e29bb439fd60f30bf9c855f6fbf9d6a09ec
Awesohame <82087247+awesohame@users.noreply.github.com>
2024-02-04 19:57:17 +0530

    added getUserChannelProfile function to user.controller.js

--- backend/src/models/subscription.model.js
+++ backend/src/models/subscription.model.js
@@ -15,4 +15,4 @@ const subscriptionSchema = new Schema({
     }
 )
 
-export const Subsciption = mongoose.model('Subscription', subscriptionSchema)
+export const Subscription = mongoose.model('Subscription', subscriptionSchema)
