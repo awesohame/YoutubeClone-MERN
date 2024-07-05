edaf613c01436a123d1f7392d171ae1c89d7135e
Awesohame <82087247+awesohame@users.noreply.github.com>
2024-07-05 21:40:55 +0530

    Update constants.js

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
 export const navHeight = "56px"
 export const sidebarWidth = "245px"
 
-export const apiURL = "https://youtube-clone-mern-server.vercel.app/"
+export const apiURL = "https://youtube-clone-mern-server.vercel.app/api/v1"
