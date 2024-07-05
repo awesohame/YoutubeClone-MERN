43d63c915d2a4487ec596ad29f060b9066659b33
Awesohame <82087247+awesohame@users.noreply.github.com>
2024-07-05 20:23:24 +0530

    vercel cors

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
 export const navHeight = "56px"
 export const sidebarWidth = "245px"
 
-export const apiURL = "http://localhost:8000/api/v1"
+export const apiURL = "https://youtube-clone-mern-server-ago67nej0-sohams-projects-fe041e91.vercel.app/"
